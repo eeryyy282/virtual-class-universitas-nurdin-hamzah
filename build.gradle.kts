33cb61ab23f05d0d80cc342c57d5e730a9e729d0
Airi <airiagustus82@gmail.com>
2025-05-14 23:29:16 +0700

    Update: Devtools KSP version to 2.1.21-2.0.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.dynamic.feature) apply false
     id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13" apply false
+    id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
 }

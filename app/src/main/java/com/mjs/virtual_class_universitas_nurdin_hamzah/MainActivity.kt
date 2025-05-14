082e32b2c858a0f72fc5915e3358ca33c35acb08
Airi <airiagustus82@gmail.com>
2025-05-14 23:30:10 +0700

    Update: Reformat, delete unused import, and optimize some code.

--- app/src/main/java/com/mjs/virtual_class_universitas_nurdin_hamzah/MainActivity.kt
+++ app/src/main/java/com/mjs/virtual_class_universitas_nurdin_hamzah/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.mjs.virtual_class_universitas_nurdin_hamzah
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mjs.virtual_class_universitas_nurdin_hamzah.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {

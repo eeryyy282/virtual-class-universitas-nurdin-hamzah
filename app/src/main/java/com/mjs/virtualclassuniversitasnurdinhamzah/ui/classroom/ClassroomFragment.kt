88ca1ab4531398fd187bca9fffec2af6d75c58d5
Airi <airiagustus82@gmail.com>
2025-05-17 06:52:55 +0700

    Refactor: Clean up ClassroomFragment and update vector drawables
    This commit removes the unused `newInstance()` method and the `viewModel` property from `ClassroomFragment.kt`.
    
    Additionally, it adds the `tools:ignore="VectorPath"` attribute to the path elements in `schedule_icon_deactive.xml`, `schedule_icon_active.xml`, and `classroom_icon_active.xml` to suppress lint warnings related to vector paths. The `xmlns:tools` namespace declaration has also been added to these files.

--- app/src/main/java/com/mjs/virtualclassuniversitasnurdinhamzah/ui/classroom/ClassroomFragment.kt
+++ app/src/main/java/com/mjs/virtualclassuniversitasnurdinhamzah/ui/classroom/ClassroomFragment.kt
@@ -5,16 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import com.mjs.virtual_class_universitas_nurdin_hamzah.R
 
 class ClassroomFragment : Fragment() {
 
     companion object {
-        fun newInstance() = ClassroomFragment()
     }
-
-    private val viewModel: ClassroomViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

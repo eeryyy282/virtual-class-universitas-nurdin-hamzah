6271cb0153c4e38b19025c3d1845e700e9da944a
Airi <airiagustus82@gmail.com>
2025-07-22 06:26:37 +0700

    Refactor: Implement theme change functionality and update MotionScene
    This commit introduces theme changing capabilities and updates the MotionScene accordingly.
    
    Key changes:
    - In `onboarding/build.gradle.kts`:
        - Enabled `viewBinding`.
    - In `onboarding/src/main/java/com/mjs/onboarding/OnboardingActivity.kt`:
        - Implemented view binding using `ActivityOnboardingBinding`.
        - Added a `changeTheme()` function that toggles between light and dark mode using `AppCompatDelegate` when `btn_change_theme` is clicked.
    - In `onboarding/src/main/res/xml/activity_onboarding_scene.xml`:
        - Added `Constraint`s for `btn_change_theme` and `iv_icon_change_theme` with `android:visibility="gone"` and `app:layout_constraintEnd_toEndOf="parent"` in `ConstraintSet`s for `scene_2`, `scene_3`, and `scene_4`.

--- onboarding/src/main/java/com/mjs/onboarding/OnboardingActivity.kt
+++ onboarding/src/main/java/com/mjs/onboarding/OnboardingActivity.kt
@@ -3,18 +3,38 @@ package com.mjs.onboarding
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mjs.onboarding.databinding.ActivityOnboardingBinding
 
 class OnboardingActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityOnboardingBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_onboarding)
+
+        binding = ActivityOnboardingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        changeTheme()
+    }
+
+    private fun changeTheme() {
+        binding.btnChangeTheme.setOnClickListener {
+            if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            }
         }
     }
 }

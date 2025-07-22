8ed3e91f52af5ee606e6d9c25decf87f6261bb5c
Airi <airiagustus82@gmail.com>
2025-07-22 17:58:12 +0700

    Refactor: Update ThemePreference and CoreModule for DataStore
    This commit updates `ThemePreference.kt` and `CoreModule.kt` to integrate `DataStore` for theme settings.
    
    Key changes:
    - In `ThemePreference.kt`:
        - Added an extension property `Context.dataStore` to initialize `DataStore<Preferences>` with the name "ThemeSetting".
    - In `CoreModule.kt`:
        - Added a Koin definition to provide `androidContext().dataStore` as a singleton.

--- core/src/main/java/com/mjs/core/di/CoreModule.kt
+++ core/src/main/java/com/mjs/core/di/CoreModule.kt
@@ -3,12 +3,15 @@ package com.mjs.core.di
 import com.mjs.core.data.repository.ThemeRepository
 import com.mjs.core.data.source.local.LocalDataSource
 import com.mjs.core.data.source.local.pref.ThemePreference
+import com.mjs.core.data.source.local.pref.dataStore
 import com.mjs.core.domain.repository.IThemeRepository
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val repositoryModule =
     module {
         single { LocalDataSource(get()) }
         single { ThemePreference(get()) }
+        single { androidContext().dataStore }
         single<IThemeRepository> { ThemeRepository(get()) }
     }

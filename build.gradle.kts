9a22b80b90819bfd1a6c2428497432697f54f14b
Airi <airiagustus82@gmail.com>
2025-07-22 12:47:40 +0700

    Refactor: Update MotionScene and Gradle plugin versions
    This commit updates the `activity_onboarding_scene.xml` and bumps the versions of the `ktlint` and `ksp` Gradle plugins.
    
    In `onboarding/src/main/res/xml/activity_onboarding_scene.xml`:
    - For `btn_change_theme` and `iv_icon_change_theme` in the `scene_2` `ConstraintSet`:
        - Changed `android:visibility` from `gone` to `invisible`.
        - Changed `app:layout_constraintEnd_toEndOf="parent"` to `app:layout_constraintBottom_toTopOf="parent"`.
    
    In `build.gradle.kts`:
    - Updated `org.jlleitschuh.gradle.ktlint` from version `11.5.1` to `13.0.0`.
    - Updated `com.google.devtools.ksp` from version `2.1.21-2.0.1` to `2.2.0-2.0.2`.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,6 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.android.dynamic.feature) apply false
-    id("org.jlleitschuh.gradle.ktlint") version "11.5.1"
-    id("com.google.devtools.ksp") version "2.1.21-2.0.1" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "13.0.0"
+    id("com.google.devtools.ksp") version "2.2.0-2.0.2" apply false
 }

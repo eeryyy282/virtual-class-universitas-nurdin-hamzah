db410d21ed8fa1541a3c22f047bfca1a3eacce13
Airi <airiagustus82@gmail.com>
2025-08-07 07:46:58 +0700

    Refactor: Initialize database on application start
    This commit updates `MyApplication.kt` to initialize the `VirtualClassDatabase` when the application is created. This ensures the database is ready for use as soon as the app launches.
    
    **Key changes:**
    
    -   **`MyApplication.kt`:**
        -   Injected `VirtualClassDatabase`.
        -   Called `database.openHelper.writableDatabase` in the `onCreate` method to trigger database initialization.

--- app/src/main/java/com/mjs/virtualclassuniversitasnurdinhamzah/MyApplication.kt
+++ app/src/main/java/com/mjs/virtualclassuniversitasnurdinhamzah/MyApplication.kt
@@ -1,16 +1,20 @@
 package com.mjs.virtualclassuniversitasnurdinhamzah
 
 import android.app.Application
+import com.mjs.core.data.source.local.room.VirtualClassDatabase
 import com.mjs.core.di.databaseModule
 import com.mjs.core.di.repositoryModule
 import com.mjs.virtualclassuniversitasnurdinhamzah.di.useCaseModule
 import com.mjs.virtualclassuniversitasnurdinhamzah.di.viewModelModule
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
 
 class MyApplication : Application() {
+    private val database: VirtualClassDatabase by inject()
+
     override fun onCreate() {
         super.onCreate()
         startKoin {
@@ -25,5 +29,6 @@ class MyApplication : Application() {
                 ),
             )
         }
+        database.openHelper.writableDatabase
     }
 }
